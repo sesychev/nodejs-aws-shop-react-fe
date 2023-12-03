bbb098538e4e6667f75ce4bcc8178e13ed3ef8b4
sesychev <sergeysychou@gmail.com>
2023-12-03 21:50:54 +0300

    task-5: edit 'import' in API_PATHS

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://yptpm2h4uh.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://5a2uo799a0.execute-api.eu-west-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev"
 };
