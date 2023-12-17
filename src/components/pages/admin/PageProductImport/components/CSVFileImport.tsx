25f0ee5ef25f5ba1132034a1470697966244ebfa
sesychev <sergeysychou@gmail.com>
2023-12-18 02:52:01 +0300

    task-7: see branch task-2

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -26,7 +26,7 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const uploadFile = async () => {
     console.log("uploadFile to", url);
     if (!file?.name) return;
-    // Get the presigned URL
+
     const response = await axios({
       method: "GET",
       url,
